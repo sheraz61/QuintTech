8bae527212b621b1d5e75fa6083b6a4abe728e28
sheraz61 <hsheraz271@gmail.com>
2025-09-11 14:15:42 +0500

    add routing and layout components with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Root from './Root'
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Root />}>
+ <Route path='' element={<Home />} />
+ <Route path='/about' element={<About />} />
+ <Route path='/service' element={<Services/>} />
+    </Route>
+  )
+)
 function App() {
+
   return (
-   <div>hello world</div>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   )
 }
 
